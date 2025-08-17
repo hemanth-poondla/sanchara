78a0e5a1e8a25f8ad5930348580bbb662d72b690
hemanth-poondla <poondlahemanth1@gmail.com>
2025-08-17 12:03:54 +0530

    bitbucket to git code movemenet

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ export default function App() {
           path="/landing"
           element={
             <ProtectedRoute>
-              <GroupChat />
+              <LandingPage />
             </ProtectedRoute>
           }
         />
